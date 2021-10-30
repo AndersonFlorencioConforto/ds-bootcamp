ee10ebd8f7d9068de2941252891130d67de37082
Anderson Conforto <afcaconforto@gmail.com>
2021-10-30 12:58:40 -0300

    CategoryRepository & CategoryService & Configurações do banco H2 criados

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -1,24 +1,24 @@
 package com.devsuperior.dscatalog.controllers;
 
 import com.devsuperior.dscatalog.models.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryController {
 
+    @Autowired
+    private CategoryService service;
 
     @GetMapping
     public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L,"Books"));
-        list.add(new Category(2L,"Games"));
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
--- backend/src/main/java/com/devsuperior/dscatalog/models/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/models/Category.java
@@ -1,11 +1,16 @@
 package com.devsuperior.dscatalog.models;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
@@ -33,16 +38,4 @@ public class Category implements Serializable {
         this.name = name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Category category = (Category) o;
-        return Objects.equals(id, category.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
