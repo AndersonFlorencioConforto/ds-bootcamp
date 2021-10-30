443c12f44d1795d8a9006c20e832c196bdb71ef3
Anderson Conforto <afcaconforto@gmail.com>
2021-10-30 13:09:41 -0300

    $ gcionando comentários na anotação @Transactional e spring.jpa.open-in-view = false

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import com.devsuperior.dscatalog.models.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
@@ -12,6 +14,9 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    //Spring garante com essa notação que isso é uma transação com o banco de dados.
+    //o readOnly = true, faz com que não trave o banco de dados,pois é apenas uma transação de leitura.
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return repository.findAll();
     }
