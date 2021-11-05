44e9022fc7c64b6782f6cc08f9a54feca6550608
Anderson Conforto <afcaconforto@gmail.com>
2021-11-04 21:50:37 -0300

    Cria classe de testes ProductServiceTests e implementa teste do deleteById sem exception

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -12,11 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,4 +1,44 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
+
+    @InjectMocks
+    private ProductService service;
+    @Mock
+    private ProductRepository repository;
+    @Mock
+    private CategoryRepository categoryRepository;
+    private Long idExistente;
+    private Long idNaoExistente;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        idExistente = 1L;
+        idNaoExistente = 1000L;
+        //Comportamento simulado do DELETE sem exception
+        Mockito.doNothing().when(repository).deleteById(idExistente);
+        //Comportamento simulado do DELETE com exception
+        Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(idNaoExistente);
+    }
+
+    @Test
+    public void delete_DeveriaNaoFazerNada_QuandoIdExistir() {
+        Assertions.assertDoesNotThrow(() -> {
+            service.delete(idExistente);
+        });
+        Mockito.verify(repository,Mockito.times(1)).deleteById(idExistente);
+    }
 }
