6191cdcaadceef59a11ce6553b116a5a7a23229d
Anderson Conforto <afcaconforto@gmail.com>
2021-11-11 19:43:51 -0300

    Refatora findAll em productControllerTests

--- backend/src/test/java/com/devsuperior/dscatalog/controllers/ProductControllerTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/controllers/ProductControllerTests.java
@@ -1,2 +1,48 @@
-package com.devsuperior.dscatalog.controllers;public class ProductControllerTests {
+package com.devsuperior.dscatalog.controllers;
+
+import com.devsuperior.dscatalog.dtos.ProductDTO;
+import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dscatalog.tests.Factory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductController.class)
+public class ProductControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
+    private PageImpl<ProductDTO> page;
+    private ProductDTO productDto;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        productDto = Factory.novoProdutoDTO();
+        page = new PageImpl<>(List.of(productDto));
+        Mockito.when(productService.findAllPaged(ArgumentMatchers.any())).thenReturn(page);
+    }
+
+    @Test
+    public void findAll_DeveriaRetornarUmaPagina() throws Exception {
+        ResultActions result =
+                mockMvc.perform(MockMvcRequestBuilders.get("/products")
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
+    }
 }
