467baad8d61f535e9135fd6c029fd19984a86c63
Anderson Conforto <afcaconforto@gmail.com>
2021-11-13 14:54:36 -0300

    Cria classes user e roles

--- backend/src/main/java/com/devsuperior/dscatalog/models/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/models/Category.java
@@ -3,6 +3,9 @@ package com.devsuperior.dscatalog.models;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -18,6 +21,8 @@ public class Category implements Serializable {
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updatedAt;
 
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -63,4 +68,20 @@ public class Category implements Serializable {
         updatedAt = Instant.now();
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
