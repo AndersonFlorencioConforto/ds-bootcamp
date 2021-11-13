0b3e95f06020baa079817256b89e1ff47acba686
Anderson Conforto <afcaconforto@gmail.com>
2021-11-13 18:34:48 -0300

    Mapeia tabela user e role, e seed do banco de dados

--- backend/src/main/java/com/devsuperior/dscatalog/models/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/models/User.java
@@ -1,19 +1,30 @@
 package com.devsuperior.dscatalog.models;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
 
+    @ManyToMany
+    @JoinTable(
+            name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     public User() {
