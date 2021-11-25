aaa0838ff108aaa5b734079ad31655068d590adf
Anderson Conforto <afcaconforto@gmail.com>
2021-11-25 20:28:54 -0300

    IntCria exception personalizada do Validation

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/ProductController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import javax.validation.Valid;
 import java.net.URI;
 
