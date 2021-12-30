12c0450c4c1c6040d3a6eea9528a6e9b1478341a
Anderson Conforto <afcaconforto@gmail.com>
2021-12-30 20:43:06 -0300

    Finalizando a estilização da tela home

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -10,8 +10,10 @@ const Home = () => {
       <div className="home-container">
           <div className="home-card">
               <div className="home-content-container">
-                <h1> Conheça o melhor catálogo de produtos</h1> 
+               <div>
+               <h1> Conheça o melhor catálogo de produtos</h1> 
                 <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
+               </div>
                 <ButtonIcon/>
               </div>
               <div className="home-image-container" >
