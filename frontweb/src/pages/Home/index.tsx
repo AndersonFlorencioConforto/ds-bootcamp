0e1f2790313110184e1af6ca1f28099d8b9bf648
Anderson Conforto <afcaconforto@gmail.com>
2021-12-30 19:47:38 -0300

    Estila e arruma responsividade da tela home até 992px

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import Navbar from 'components/Navbar';
 import { ReactComponent as MainImage} from 'assets/images/main-image.svg'
+import './styles.css'
+import ButtonIcon from 'components/ButtonIcon';
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
           <div className="home-card">
               <div className="home-content-container">
                 <h1> Conheça o melhor catálogo de produtos</h1> 
+                <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
+                <ButtonIcon/>
               </div>
               <div className="home-image-container" >
                     <MainImage/>
